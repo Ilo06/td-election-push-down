409ee1ea3c54a094b016d39897bf681e7480e04d
Ilo <hei.iloniaina@gmail.com>
2026-02-19 16:30:09 +0300

    feat: implements q2 countVotesByType

--- src/main/java/DataRetriever.java
+++ src/main/java/DataRetriever.java
@@ -2,6 +2,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class DataRetriever {
     public long countAllVotes() {
@@ -23,6 +24,34 @@ public class DataRetriever {
         } catch (SQLException e) {
             throw new RuntimeException(e);
 
+        }
+    }
+
+    public List<VoteTypeCount> countVotesByType() {
+        String sql = """
+                SELECT vote_type, COUNT(*) AS count
+                FROM vote
+                GROUP BY vote_type
+                ORDER BY vote_type;
+                
+                """;
+
+        try (Connection connection = new DBConnection().getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            List<VoteTypeCount> result = new java.util.ArrayList<>();
+
+            while (rs.next()) {
+                VoteType voteType = VoteType.valueOf(rs.getString("vote_type"));
+                int count = rs.getInt("count");
+                result.add(new VoteTypeCount(voteType, count));
+            }
+
+            return result;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         DataRetriever dataRetriever = new DataRetriever();
-        System.out.println(dataRetriever.countAllVotes());
+//        System.out.println(dataRetriever.countAllVotes());
+
+        System.out.println(dataRetriever.countVotesByType());
+
     }
 }
